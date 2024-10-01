35782d86b1f2490aa2358892643f552219daa630
ChoiGyeongJu <rudwn5142@naver.com>
2024-10-01 14:49:50 +0900

    chore: react hook form, quill 패키지 추가

--- src/desktop/components/PostDetail/fragments/PostContents.tsx
+++ src/desktop/components/PostDetail/fragments/PostContents.tsx
@@ -1,4 +1,5 @@
-import DOMPurify from 'dompurify';
+import ReactQuill from 'react-quill';
+
 import styled from 'styled-components';
 
 interface Props {
@@ -8,7 +9,7 @@ interface Props {
 const PostContents = ({ contents }: Props) => {
   return (
     <ContentWrapper>
-      <div dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(contents) }} />
+      <ReactQuill readOnly value={contents} modules={{ toolbar: false }} className="contents" />
     </ContentWrapper>
   );
 };
@@ -18,4 +19,12 @@ export default PostContents;
 const ContentWrapper = styled.div`
   padding: 20px 0;
   border-top: 1px solid lightgray;
+
+  & .contents {
+    height: auto;
+    min-height: 300px;
+    & .ql-snow {
+      border: none;
+    }
+  }
 `;
